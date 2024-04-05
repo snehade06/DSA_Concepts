da965e5c6b6636352acf1ac8a776e98622a86243
snehade06 <149817862+snehade06@users.noreply.github.com>
2024-04-05 19:18:56 +0530

    Create Vector_erase.cpp

---
+++ Vector_erase.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    int e;
+    cin>>e;
+    v.erase(v.begin()+(e-1));
+    int v1,v2;
+    cin>>v1>>v2;
+    v.erase(v.begin()+(v1-1),v.begin()+(v2-1));
+    cout<<v.size()<<endl;
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    return 0;
+}
